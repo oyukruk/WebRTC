4fc1cbf2a99f61559f7e2ed330d02a18d0abe795
G-Dogdu <69244524+G-Dogdu@users.noreply.github.com>
2020-08-12 15:22:09 +0300

    Update app.js
    update answer

--- SignalServer/app.js
+++ SignalServer/app.js
@@ -77,34 +77,42 @@ console.log("Listening for incoming connections on port:" + expressServerPort);
 io.on('connection', (socket) => {
   socket.emit("welcome");
   console.log('a user connected');
+  //offer olan durumda answer geçişi için
+  var isOffered = true;
+  //answerda kullanmak için globale çektim
+  var callerUserObject = 0;
+  var offeredUserObject = 0;
 
   socket.on('login', (username) => {
     var userObject = {};
-    userObject.username = username;    
+    userObject.username = username;
     userObject.socketId = socket.id;
-
     var generatedUsername = "Anonymous";
+    
 
     if (userObject.username == "") {
       socket.emit("emptyUsername", generatedUsername);
       userObject.username = generatedUsername;
-      usersArray.push(userObject);      
-    } 
-    else {
-   
-      for (let index = 0; index < usersArray.length; index++) {
-      var nextUser = usersArray[index];
-      if (nextUser.username == userObject.username && nextUser.username != generatedUsername) {
-        socket.emit("signalServerError", {errorDescription: "Baglanmak istenen kullanıcı adına sahip bir kullanıcı zaten bulunuyor.", errorCode:"ERR-USER-003"});
-        return;
-      }
-    }
+      usersArray.push(userObject);
+    } else {
 
-    usersArray.push(userObject);
-    socket.emit("saved user:" + userObject.username);
-    
-  }});
-    
+      for (let index = 0; index < usersArray.length; index++) {
+        var nextUser = usersArray[index];
+        if (nextUser.username == userObject.username && nextUser.username != generatedUsername) {
+          socket.emit("signalServerError", {
+            errorDescription: "Baglanmak istenen kullanıcı adına sahip bir kullanıcı zaten bulunuyor.",
+            errorCode: "ERR-USER-003"
+          });
+          return;
+        }
+      }
+
+      usersArray.push(userObject);
+      socket.emit("saved user:" + userObject.username);
+
+    }
+  });
+
   socket.on("disconnect", function () {
     var foundUserObjectToRemove = false;
     var userObjectToBeRemoved = -1;
@@ -128,72 +136,78 @@ io.on('connection', (socket) => {
 
   socket.on("offer", function (usernameToCall) {
 
-    var callerUserObject = 0;
-    var offeredUserObject = 0;
 
     //elimizdeki arrayde dönüyoruz
     for (let index = 0; index < usersArray.length; index++) {
-        var user = usersArray[index];
-        //eğer arraydeki user objelerinden, socket id'si bu event'e gelen socket id ile eşitse
-        //ilgili user objesi, bu eventi tetikleyen; yani offerı yapan user oluyor. (arayan kişi)
-        if(user.socketId == socket.id)
-        {
-          callerUserObject = user;
-        }
+      var user = usersArray[index];
+      //eğer arraydeki user objelerinden, socket id'si bu event'e gelen socket id ile eşitse
+      //ilgili user objesi, bu eventi tetikleyen; yani offerı yapan user oluyor. (arayan kişi)
+      if (user.socketId == socket.id) {
+        callerUserObject = user;
+      }
 
-        //eğer arraydeki user objelerinden, username'i bu evente parametre olarak gelen usernameToCall'a eşit ise
-        //ilgili user objesi, aranmak istenen usera denk geliyor
-        if(user.username == usernameToCall)
-        {
-          offeredUserObject = user;
-        }
+      //eğer arraydeki user objelerinden, username'i bu evente parametre olarak gelen usernameToCall'a eşit ise
+      //ilgili user objesi, aranmak istenen usera denk geliyor
+      if (user.username.toLowerCase() == usernameToCall.toLowerCase()) {
+        offeredUserObject = user;
+      }
 
-
-        if(!callerUserObject && !offeredUserObject)
-          break;
+      if (!callerUserObject && !offeredUserObject)
+        break;
     }
 
     //loop bitti, fakat aramayı yapmak isteyen kullanıcının objesini arrayde bulamadım.
-    if(!callerUserObject)
-    {
+    if (!callerUserObject) {
       var errorObject = {};
       errorObject.errorDescription = "Aramayı yapmak isteyen kullanıcıyı bulamadım, lütfen tekrar giriş yapınız.";
       errorObject.errorCode = "ERR-USER-001";
       socket.emit("signalServerError", errorObject);
-
+      isOffered = false;
       console.log("callUserObject is null, terminating.");
       return;
     }
 
     //loop bitti, fakat aranmak istenen kullanıcının objesini arrayde bulamadım.
-    if(!offeredUserObject)
-    {
+    if (!offeredUserObject) {
       var errorObject = {};
       errorObject.errorDescription = "Aranmak istenen kullanıcıyı bulamadım, lütfen daha sonra tekrar deneyiniz.";
       errorObject.errorCode = "ERR-USER-002";
       socket.emit("signalServerError", errorObject);
-
+      isOffered = false;
       console.log("offeredUserObject is null, terminating.");
       return;
     }
 
     //Ece'nin Mert'i aramak istediği senaryoda:
     //MertUser.socket.emit('offer', EceUser.username)    
-    io.of("/").connected[offeredUserObject.socketId].emit("offer",callerUserObject.username);
-  });
-  
-  socket.on("answer", function() {
-            
+    io.of("/").connected[offeredUserObject.socketId].emit("offer", callerUserObject.username);
   });
 
-  socket.on("candidate", function(){
+  socket.on("answer", function () {
+
+    if(isOffered)
+    {
+      socket.emit("answer",callerUserObject.username);
+      console.log("answering : " + callerUserObject.username);
+    }
+    else
+    {
+      var errorObject = {};
+      errorObject.errorDescription = "Cevaplanmak istenen kullanıcıyı bulamadım, lütfen daha sonra tekrar deneyiniz.";
+      errorObject.errorCode = "ERR-USER-003";
+      socket.emit("signalServerError", errorObject);
+      console.log("can't answer, terminating.");
+    }
+  });
+
+  socket.on("candidate", function () {
 
   });
 });
 
 
 function removeElement(array, elem) {
-  console.log("removing object from array. username:" + elem.username+" socketId:"+elem.socketId);
+  console.log("removing object from array. username:" + elem.username + " socketId:" + elem.socketId);
   var index = array.indexOf(elem);
   if (index > -1) {
     array.splice(index, 1);
